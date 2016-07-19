6cc2e6fbc69feaa1e76dacce39757f3fe9262849
ili <ili@WS-ILI1UA1>
2016-07-19 15:29:44 +0300

    fix

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -3,13 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Task6
 {
     class Program
     {
+        /// <summary>
+        /// Закомментарена реализация исходного .exe при которой образуется deadLock
+        /// </summary>
+        
+        //private static object knife = new object();
+        //private static object cuttingBoard = new object();
+        private static object kitchenStuff = new object();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Girls are going to start cooking....");
+           
+            Thread wife = new Thread(WifeCoocks);
+            Thread mother = new Thread(MotherCoocks);
+            wife.Start();
+            mother.Start();
+           
+            Console.ReadLine();
+        }
+
+        public static void WifeCoocks()
+        {
+            lock (kitchenStuff)
+            {
+                Console.WriteLine("Wife took the knife!");
+                Console.WriteLine("Wife would like to take a cutting board....");
+              
+                //Thread.Sleep(1);
+                //lock (cuttingBoard)
+                //{
+                    Console.WriteLine("Wife took the cuttingBoard!");
+
+                // }
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        public static void MotherCoocks()
+        {
+
+            lock (kitchenStuff)
+            {
+                Console.WriteLine("Mother took the cuttingBoard!");
+                Console.WriteLine("Mother would like to take the knife....");
+                //Thread.Sleep(1);
+                //lock (knife)
+                //{
+                    Console.WriteLine("Mother took the knife!");
+
+                Thread.Sleep(1000);
+                //}
+            }
+
+           
         }
     }
 }
